9a05813f6d2c0a0801af2023f510e65dd8631a40
JaneKodJava <evgeniaorlova510@gmail.com>
2021-03-06 15:54:40 +0300

    Experiments on cats

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -1,10 +1,46 @@
+import java.util.concurrent.Callable;
 
 public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+        Cat murka = new Cat();
+        Cat pusha = new Cat();
+        Cat barsik = new Cat();
+        Cat vasya = new Cat();
+        Cat bonifaciy = new Cat();
 
-        System.out.println(cat.getStatus());
+        System.out.println("Murka: " +murka.getStatus());
+        System.out.println("Murka weight: " +murka.getWeight());
+        murka.meow();
+        System.out.println("Murka weight: " +murka.getWeight());
+        murka.feed(200.0);
+        System.out.println("Murka weight: " +murka.getWeight());
+        System.out.println("Murka: " +murka.getStatus());
+        System.out.println("======================================================");
+
+        System.out.println("Pusha: " +pusha.getStatus());
+        System.out.println("Pusha weight: " +pusha.getWeight());
+        pusha.feed(150.50);
+        System.out.println("======================================================");
+
+        System.out.println("Barsik: " +barsik.getStatus());
+        System.out.println("Barsik weight: " +barsik.getWeight());
+        System.out.println("======================================================");
+
+        System.out.println("Vasya: " +vasya.getStatus());
+        System.out.println("Vasya weight: " +vasya.getWeight());
+        vasya.feed(10000.0);
+        System.out.println("Vasya: " +vasya.getStatus());
+        System.out.println("Vasya weight: " +vasya.getWeight());
+        System.out.println("======================================================");
+
+        System.out.println("Bonifaciy weight: " +bonifaciy.getWeight());
+        System.out.println("Bonifaciy weight: " +bonifaciy.getWeight());
+        while (bonifaciy.getWeight() > 999){
+            bonifaciy.meow();
+        }
+        System.out.println("Bonifaciy : " +bonifaciy.getStatus());
+        System.out.println("======================================================");
     }
 }
