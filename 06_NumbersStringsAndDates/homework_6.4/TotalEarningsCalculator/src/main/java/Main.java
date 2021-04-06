c25f9636b86e8e65ab6f9408fbd772a962c1874c
JaneKodJava <evgeniaorlova510@gmail.com>
2021-04-06 15:51:29 +0300

    Domashka 6.4 Salary

--- 06_NumbersStringsAndDates/homework_6.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -4,6 +4,29 @@ public class Main {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
     //TODO: напишите ваш код, результат вывести в консоль
+
+    System.out.println("Текст = " + text);
+
+    System.out.println("Номер цифры 5: " + text.indexOf('5'));
+    String vasyaSalaryIndex = text.substring(15,19);
+    System.out.println("Васина зарплата: " + vasyaSalaryIndex);
+
+    System.out.println("Номер цифры 7: " + text.indexOf('7'));
+    String petyaSalary = text.substring(35,39);
+    System.out.println("Петина зарплата: " + petyaSalary);
+
+    System.out.println(text.lastIndexOf('3'));
+    String mashaSalary = text.substring(56,61);
+    System.out.println("Машина зарплата: " +mashaSalary);
+
+    Integer vasyaZp = Integer.parseInt(vasyaSalaryIndex);
+    Integer petyaZp = Integer.parseInt(petyaSalary);
+    Integer mashaZp = Integer.parseInt(mashaSalary);
+
+    int sum = vasyaZp + petyaZp + mashaZp;
+    System.out.println("Сумма: " + sum);
+
+
   }
 
 }
