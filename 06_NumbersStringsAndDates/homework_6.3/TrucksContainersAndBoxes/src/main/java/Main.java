d56b2f1e349dfbd68d065808282178714d720fbe
JaneKodJava <evgeniaorlova510@gmail.com>
2021-04-15 12:22:35 +0300

    Trucks

--- 06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -2,12 +2,39 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static final int MAX_CONTAINER = 12;
+    public static final int MAX_BOXES = 27;
+    public static int countTruck;
+    public static int countContainer;
+
     public static void main(String[] args) {
-        System.out.println("Введите количество ящиков: ");
 
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
 
+        if (Integer.parseInt(boxes) % MAX_BOXES != 0){
+            countContainer = Integer.parseInt(boxes) / MAX_BOXES + 1;
+        }
+        else {
+            countContainer = Integer.parseInt(boxes) / MAX_BOXES;
+        }
+        if (countContainer % MAX_CONTAINER != 0){
+            countTruck = countContainer / MAX_CONTAINER + 1;
+        }
+        else {
+            countTruck = countContainer / MAX_CONTAINER;
+        }
+
+        for (int i = 0; i < Integer.parseInt(boxes); i++) {
+            if (i % (MAX_BOXES * MAX_CONTAINER) == 0){
+                System.out.println("Грузовик: " + (i / (MAX_CONTAINER * MAX_BOXES) + 1));
+            }
+            if (i % MAX_BOXES == 0){
+                System.out.println("\tКонтейнер: " + ((i/MAX_BOXES) + 1));
+            }
+            System.out.println("\t\tЯщик: " + (i + 1));
+        }
+        System.out.println("Необходимо:" + "\r\nгрузовиков - " + countTruck + " шт.\nконтейнеров - " + countContainer + " шт.");
 
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
         // пример вывода при вводе 2
