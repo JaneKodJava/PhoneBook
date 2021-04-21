aa2177e9b9d8089503b6b4be136f771c0ef270c4
JaneKodJava <evgeniaorlova510@gmail.com>
2021-04-21 16:08:09 +0300

    Masking regex

--- 06_NumbersStringsAndDates/homework_6.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 06_NumbersStringsAndDates/homework_6.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -1,12 +1,22 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        String text = "Номер кредитной карты: <4042 1234 5678> 7890";
+        searchAndReplaceDiamonds(text, "***");
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+        Pattern pattern = Pattern.compile("<.+>");
+        Matcher matcher = pattern.matcher(text);
+        String masking = matcher.replaceAll(placeholder);
+        System.out.println(masking);
+
         return text;
     }
+
 
 }
