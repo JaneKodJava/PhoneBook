74747eed51cbde6149eb6172dbf0fe16ae267f04
JaneKodJava <evgeniaorlova510@gmail.com>
2021-04-28 15:36:06 +0300

    Reverse Array

--- 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/Main.java
@@ -1,6 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
+
         String line = "Каждый охотник желает знать, где сидит фазан";
+        String[] color = line.split(",?\\s+");
+
+        //String[] colorInverted = new String[color.length];
+        ReverseArray.reverse(color);
+
+        //for (int i = color.length - 1, j = 0; i >= 0; i--, j++) {
+        //    temp = colorInverted[j];
+        //    colorInverted[j]  = color[i];
+        //    color[i] = temp;
+        //}
+        for (int i = 0; i < color.length; i++) {
+            System.out.println(color[i]);
+        }
+
+
     }
 }
--- 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/ReverseArray.java
+++ 07_ArraysAndCollections/homework_7.1/ReverseArray/src/main/java/ReverseArray.java
@@ -2,6 +2,12 @@ public class ReverseArray {
 
     //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
     public static String[] reverse (String[] strings){
+        int size = strings.length;
+        for (int i = 0; i < size/2; i++) {
+            String temp = strings[i];
+            strings[i] = strings[size -1 -i];
+            strings[size -1 -i] = temp;
+        }
         return strings;
     }
 }
