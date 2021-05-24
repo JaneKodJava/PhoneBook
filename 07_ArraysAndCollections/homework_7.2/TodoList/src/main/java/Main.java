14aec76259f3ee9ce5c9d3fa85321bc8b2edb960
JaneKodJava <evgeniaorlova510@gmail.com>
2021-05-24 15:37:21 +0300

    TO DO List

--- 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,73 @@
+import net.sf.saxon.pattern.Pattern;
+
+import java.util.Scanner;
+
 public class Main {
+
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
+        System.out.println("Привет! Сегодня тебя ждут великие дела!" +
+                "\nНу мы-то с тобой знаем, что это значит на самом деле;)" +
+                "\nВведи команду заглавными буквами и дело строчными, например:" +
+                "\nADD 1 запихнуть грязную посуду в посудомойку" +
+                "\nВот команды, которые я выполняю: ADD, EDIT, DELETE, LIST" +
+                "\nИ очень тебя прошу, пожалей мою память!" +
+                "\nПрежде, чем что-то написать, хорошо подумай))) тебе же потом это делать придётся...");
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            String input = scanner.nextLine();
+            String text = input.trim();
+            int countSpace = 0;
+            for (char space : text.toCharArray()) {
+                if (space == ' ') countSpace = countSpace + 1;
+            }
+
+            if (countSpace > 0){
+                int spaceIndex = text.indexOf(' ');
+                String command = text.substring(0, spaceIndex); // команда
+                String thing = text.substring(spaceIndex +1); // текст дела
+                if (command.equals("ADD")) {
+                    if (!indexEntered(thing)){
+                        String spaceThing = thing.trim();
+                        int spaceIndexThing = spaceThing.indexOf(' ');
+                        String numberThing = spaceThing.substring(0, spaceIndexThing); // номер дела
+                        String todo = spaceThing.substring(spaceIndexThing +1); // текст дела с номером
+                        int index = Integer.parseInt(numberThing);
+                        todoList.add(index, todo);
+                    }
+                    else {
+                        todoList.add(thing);
+                    }
+
+                }
+                if (command.equals("EDIT")){
+                    String spaceThing = thing.trim();
+                    int spaceIndexThing = spaceThing.indexOf(' ');
+                    String numberThing = spaceThing.substring(0, spaceIndexThing); // номер дела
+                    String todo = spaceThing.substring(spaceIndexThing +1); // текст дела с номером
+                    int index = Integer.parseInt(numberThing);
+                    todoList.edit(todo, index);
+                }
+                if (command.equals("DELETE")){
+                    int index = Integer.parseInt(thing);
+                    todoList.delete(index);
+                }
+
+
+            }
+            if (countSpace == 0){
+                String command = text;
+                if (command.equals("LIST")){
+                    todoList.getTodos();
+                }
+            }
+
+        }
     }
+    public static boolean indexEntered(String text){
+        return text.matches("[^0-9]+");
+    }
+
 }
--- 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
+++ 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
@@ -2,28 +2,72 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    ArrayList<String> arrayList = new ArrayList<>();
+
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
+        arrayList.add(todo);
+
+        if (arrayList.size() <= 10){
+            System.out.println("Отлично! Так держать! " +
+                    "\nТы добавил дело " + todo);
+        }
+        else if (arrayList.size() > 10 && arrayList.size() < 15){
+            System.out.println("Ого! Сколько дел!");
+        }
+        else {
+            System.out.println("Давай что-нибудь перенесём на завтра...");
+        }
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
+
+        if (index > arrayList.size()){
+            arrayList.add(todo);
+            System.out.println("Номер не существует. Дело добавлено в конец списка.");
+        }
+        else if (index < arrayList.size()){
+            arrayList.add(index, todo);
+            System.out.println("Дело добавлено под номером " + index + ": " + todo);
+        }
+
     }
 
     public void edit(String todo, int index) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
+
+        if (arrayList.size() > index){
+            arrayList.set(index, todo);
+            System.out.println("Ты заменил дело номер " + index + " на: " + todo);
+        }
+        if (arrayList.size() < index){
+            System.out.println("Номер не существует");
+        }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
+
+        if (arrayList.size() > index){
+            arrayList.remove(index);
+            System.out.println("Ты удалил дело номер: " + index);
+        }
+        else {
+            System.out.println("Данный индекс отсутствует. Дело удалить невозможно.");
+        }
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        return new ArrayList<>();
+        System.out.println("Список дел:");
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+        return arrayList;
     }
 
 }
