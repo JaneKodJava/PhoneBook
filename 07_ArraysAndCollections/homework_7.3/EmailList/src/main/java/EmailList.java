8d0848a913ed99b25ccef5696f10e96cb0269e4e
JaneKodJava <evgeniaorlova510@gmail.com>
2021-06-01 19:27:28 +0300

    Email List test go

--- 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/EmailList.java
+++ 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/EmailList.java
@@ -8,24 +8,31 @@ public class EmailList {
 
     public void add(String email) {
         // TODO: валидный формат email добавляется
-        char[] chars = email.toCharArray();
-        char[] caseChars = new char[chars.length];
+        if (validEMail1(email)){
+            char[] chars = email.toCharArray();
+            char[] caseChars = new char[chars.length];
 
-        for (int i = 0; i < chars.length; i++) { // Замена заглавных букв на строчные
-            caseChars[i] = Character.toLowerCase(chars[i]);
+            for (int i = 0; i < chars.length; i++) { // Замена заглавных букв на строчные
+                caseChars[i] = Character.toLowerCase(chars[i]);
+            }
+            String caseIgnoreEmail = new String(caseChars);
+            emailContacts.add(caseIgnoreEmail);
+            System.out.println("э-мэил " + caseIgnoreEmail + " добавлен в список");
         }
-        String caseIgnoreEmail = new String(caseChars);
-        emailContacts.add(caseIgnoreEmail);
-        System.out.println("э-мэил " + caseIgnoreEmail + " добавлен в список");
+
     }
 
-    public List<String> getSortedEmails() {
+    public TreeSet<String> getSortedEmails() {
         // TODO: возвращается список электронных адресов в алфавитном порядке
         System.out.println("Список контактов:");
         for (String mail : emailContacts){
             System.out.println(mail);
         }
-        return Collections.emptyList();
+        //return Collections.emptyList();
+        return emailContacts;
+    }
+    public static boolean validEMail1(String mail){
+        return mail.matches(".+@.+\\..+");
     }
 
 }
