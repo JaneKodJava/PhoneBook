ddf25c266cc5f7530a2e3fa6b84002e3ba734885
JaneKodJava <evgeniaorlova510@gmail.com>
2021-06-01 18:13:08 +0300

    Email List

--- 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/EmailList.java
+++ 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/EmailList.java
@@ -1,14 +1,30 @@
 import java.util.Collections;
 import java.util.List;
+import java.util.TreeSet;
 
 public class EmailList {
 
+    TreeSet<String> emailContacts = new TreeSet<>();
+
     public void add(String email) {
         // TODO: валидный формат email добавляется
+        char[] chars = email.toCharArray();
+        char[] caseChars = new char[chars.length];
+
+        for (int i = 0; i < chars.length; i++) { // Замена заглавных букв на строчные
+            caseChars[i] = Character.toLowerCase(chars[i]);
+        }
+        String caseIgnoreEmail = new String(caseChars);
+        emailContacts.add(caseIgnoreEmail);
+        System.out.println("э-мэил " + caseIgnoreEmail + " добавлен в список");
     }
 
     public List<String> getSortedEmails() {
         // TODO: возвращается список электронных адресов в алфавитном порядке
+        System.out.println("Список контактов:");
+        for (String mail : emailContacts){
+            System.out.println(mail);
+        }
         return Collections.emptyList();
     }
 
--- 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.3/EmailList/src/main/java/Main.java
@@ -17,7 +17,13 @@ public class Main {
         "Неверный формат email"
     */
 
+
+
     public static void main(String[] args) {
+
+        EmailList emailList = new EmailList();
+
+        System.out.println("Введите команду:");
         Scanner scanner = new Scanner(System.in);
         
         while (true) {
@@ -25,9 +31,45 @@ public class Main {
             if (input.equals("0")) {
                 break;
             }
-            
-            //TODO: write code here
-            
+            String validInput = input.trim();
+            int countSpace = 0;
+            for (char space : validInput.toCharArray()){ // Подсчет пробелов
+                if (space == ' ') countSpace++;
+            }
+            if (countSpace == 0){
+                String command = validInput;
+                switch (command){
+                    case "LIST":
+                        emailList.getSortedEmails();
+                        break;
+                    default:
+                        System.out.println("Команда введена неверно");
+                }
+            }
+
+            else if (countSpace > 0){ // Проверка на валидность э-маил
+
+                if (validEMail(input)){
+                    int spaceIndex = validInput.indexOf(' ');// индекс пробела
+                    String command = validInput.substring(0, spaceIndex); // команда
+                    String email = validInput.substring(spaceIndex +1); // э-мэил
+                    switch (command){
+                        case "ADD":
+                            emailList.add(email);
+                            break;
+                        default:
+                            System.out.println("Команда введена не6верно");
+                    }
+                }
+                else if (!validEMail(input)){
+                    System.out.println(WRONG_EMAIL_ANSWER);
+                }
+            }
+
         }
+    }
+
+    public static boolean validEMail(String mail){
+        return mail.matches(".+@.+\\..+");
     }
 }
